711f7633ef801a7c771952dc4b740976a058cc4f
ebruchez <ebruchez@orbeon.com>
2015-04-22 21:08:11 -0700

    Simple parsing demo and test

---
+++ src/test/scala/org/orbeon/darius/ParseTest.scala
@@ -0,0 +1,33 @@
+/**
+ * Copyright 2015 Orbeon, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.orbeon.darius
+
+import org.orbeon.darius.api.API
+import org.orbeon.darius.demo.XMLEventCollector
+import utest._
+import utest.framework.TestSuite
+
+object ParseTest extends TestSuite {
+
+  override def tests = TestSuite {
+    'SimpleParse {
+      val collector = new XMLEventCollector
+      API.parseString("""<simple foo="bar" xml:x="y" baz:gaga="aaa" xmlns:baz="http://baz.com/"/>""", collector)
+      println(collector.events map (_.toString) mkString "\n")
+    }
+  }
+}
